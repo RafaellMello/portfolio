72c9734281db8fa528a9ec15ac5f5861d0bd8a4e
RafaellMello <rafaellpmello@gmail.com>
2025-07-16 10:16:09 -0300

    Alterações - Rafael - 16/07/2025
    xd

--- scripts/lazyloading.js
+++ scripts/lazyloading.js
@@ -1,20 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const observer = new IntersectionObserver((entries, observer) => {
+    // Para a seção de planos
+    const planObserver = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                // Adiciona a classe "animate" quando o item entra na tela
                 entry.target.classList.add('animate');
-                observer.unobserve(entry.target); // Para de observar após a animação
+                observer.unobserve(entry.target);
             }
         });
     }, {
-        threshold: 0.5 // 50% do item precisa estar visível para disparar a animação
+        threshold: 0.25
     });
 
-    // Seleciona todos os itens de portfólio para observar
+    // Para os itens do portfólio
+    const portfolioObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.5
+    });
+
+    const planSection = document.querySelectorAll('.plans-section');
     const portfolioItems = document.querySelectorAll('.portfolio-item');
 
-    portfolioItems.forEach(item => {
-        observer.observe(item); // Inicia a observação de cada item
-    });
+    planSection.forEach(section => planObserver.observe(section));
+    portfolioItems.forEach(item => portfolioObserver.observe(item));
 });
